a14106d9ef6f341d08908adf2334925c6aa1e7af
gerald <oforige@gmail.com>
2022-07-04 10:46:57 +0000

    used the state and effect hooks to get data for contacts

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import React, {Fragment, useEffect, useState} from 'react';
+import axios from 'axios';
+import {Col, Row } from 'react-bootstrap';
 
 const Contacts = () => {
+
+  useEffect(() => {
+    getContacts();
+  }, [])
+  
+    const [contacts, setContacts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const getContacts = async () => {
+        try {
+            const res = await axios.get("https://randomuser.me/api/?results=12");
+            setContacts(res.data.results);
+            setLoading(true);
+        } catch (err) {
+            alert(err.message);
+        }
+    }
   return (
-    <div>Contacts</div>
+    <div></div>
   )
 }
 
