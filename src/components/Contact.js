18629d7f31a6d09f12fc14713f7fe84a8004f280
gerald <oforige@gmail.com>
2022-07-05 23:33:27 +0000

    added a card component to view the images and names of contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
-const Contact = () => {
+const Contact = ({ contact }) => {
   return (
-    <div>Contact</div>
+    <>
+    <Card className='my-3 p3 rounded h-90'>
+      <Card.Img style={{objectFit:"contain"}} variant='top' src={contact.picture.large} />
+      <Card.Body>
+        <Card.Title>
+          {`${contact.name.first + " " + contact.name.last}`}
+        </Card.Title>
+        <Card.Subtitle className='mb-2 text-muted'>Address</Card.Subtitle>
+        <Card.Text>
+          <div>
+            <strong>Number: </strong> {contact.location.street.number}
+          </div>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </>
   )
 }
 
